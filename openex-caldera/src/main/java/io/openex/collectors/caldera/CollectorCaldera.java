26bd2b962699cd78723d0746f8d0f62be2b983a5
Romuald Lemesle <romuald.lemesle@filigran.io>
2024-01-22 15:03:14 +0100

    [caldera] Collector for available assets (#4)

---
+++ openex-caldera/src/main/java/io/openex/collectors/caldera/CollectorCaldera.java
@@ -0,0 +1,32 @@
+package io.openex.collectors.caldera;
+
+import io.openex.collectors.caldera.client.CollectorCalderaClient;
+import io.openex.collectors.caldera.config.CollectorCalderaConfig;
+import io.openex.collectors.caldera.service.CollectorCalderaService;
+import io.openex.service.AssetEndpointService;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@RequiredArgsConstructor
+@Service
+public class CollectorCaldera {
+
+  private final CollectorCalderaConfig config;
+  private final TaskScheduler taskScheduler;
+  private final CollectorCalderaClient client;
+  private final AssetEndpointService assetEndpointService;
+
+  @PostConstruct
+  public void init() {
+    // If enabled, scheduled every X seconds
+    if (this.config.isEnable()) {
+      CollectorCalderaService service = new CollectorCalderaService(this.client, this.config, this.assetEndpointService);
+      this.taskScheduler.scheduleAtFixedRate(service, Duration.ofSeconds(this.config.getInterval()));
+    }
+  }
+
+}
---
+++ openex-caldera/src/main/java/io/openex/collectors/caldera/config/CollectorCalderaConfig.java
@@ -0,0 +1,37 @@
+package io.openex.collectors.caldera.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import jakarta.validation.constraints.NotBlank;
+
+@Setter
+@Component
+@ConfigurationProperties(prefix = "collector.caldera")
+public class CollectorCalderaConfig {
+
+  private final static String REST_V2_URI = "/api/v2";
+
+  @Getter
+  private boolean enable;
+
+  @Getter
+  @NotBlank
+  private String id;
+
+  @NotBlank
+  private String url;
+
+  @Getter
+  @NotBlank
+  private String apiKey;
+
+  @Getter
+  private int interval = 60;
+
+  public String getRestApiV2Url() {
+    return url + REST_V2_URI;
+  }
+}
