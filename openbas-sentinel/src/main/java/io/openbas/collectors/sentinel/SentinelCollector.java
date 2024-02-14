1eaf04b7398a4e3acb6f57a02e8b701f69989a02
Stephanya Casanova <stephanya.casanova@filigran.io>
2024-02-14 19:36:31 +0100

    [backend] Refact

--- openbas-sentinel/src/main/java/io/openbas/collectors/sentinel/SentinelCollector.java
+++ openbas-sentinel/src/main/java/io/openbas/collectors/sentinel/SentinelCollector.java
@@ -17,30 +17,31 @@ public class SentinelCollector {
 
         //Retrieve alerts
         String alerts = sentinelRestApiCaller.get(HttpMethod.GET, ResourceType.ALERT_RULES, Strings.EMPTY, Optional.empty());
-        System.out.println(alerts);
+        //System.out.println(alerts);
 
         //Retrieve actions from alert rules
 
 
         //Retrieve incidents
         String incidents = sentinelRestApiCaller.get(HttpMethod.GET, ResourceType.INCIDENTS, Strings.EMPTY, Optional.empty());
-        System.out.println(incidents);
+        //System.out.println(incidents);
 
         //Retrieve SecurityAlerts from incidents
-        String incident0a = sentinelRestApiCaller.get(HttpMethod.GET, ResourceType.INCIDENTS, "6055057e-e14c-4d89-9fce-344dea636534", Optional.of(ResourceType.RELATIONS));
-        System.out.println(incident0a);
+        String incident0a = sentinelRestApiCaller.get(HttpMethod.GET, ResourceType.INCIDENTS, "57e35aff-e61f-4f3b-b15d-2138743f365e", Optional.of(ResourceType.RELATIONS));
+        System.out.println("relations : "+ incident0a);
 
-        String incident0b = sentinelRestApiCaller.get(HttpMethod.POST, ResourceType.INCIDENTS, "6055057e-e14c-4d89-9fce-344dea636534", Optional.of(ResourceType.ALERTS));
-        System.out.println(incident0b);
+        String incident0b = sentinelRestApiCaller.get(HttpMethod.POST, ResourceType.INCIDENTS, "57e35aff-e61f-4f3b-b15d-2138743f365e", Optional.of(ResourceType.ALERTS));
+        System.out.println("alerts : "+ incident0b);
 
-        String incident0c = sentinelRestApiCaller.get(HttpMethod.POST, ResourceType.INCIDENTS, "6055057e-e14c-4d89-9fce-344dea636534", Optional.of(ResourceType.BOOKMARKS));
-        System.out.println(incident0c);
+        String incident0c = sentinelRestApiCaller.get(HttpMethod.POST, ResourceType.INCIDENTS, "57e35aff-e61f-4f3b-b15d-2138743f365e", Optional.of(ResourceType.BOOKMARKS));
+        //System.out.println(incident0c);
 
-        String incident0d = sentinelRestApiCaller.get(HttpMethod.POST, ResourceType.INCIDENTS, "6055057e-e14c-4d89-9fce-344dea636534", Optional.of(ResourceType.ENTITIES));
-        System.out.println(incident0d);
+        //Get entities from a incidente !! https://learn.microsoft.com/fr-fr/rest/api/securityinsights/incidents/list-entities?view=rest-securityinsights-2023-11-01&tabs=HTTP#filehashentity
+        String incident0d = sentinelRestApiCaller.get(HttpMethod.POST, ResourceType.INCIDENTS, "57e35aff-e61f-4f3b-b15d-2138743f365e", Optional.of(ResourceType.ENTITIES));
+        System.out.println("entities : "+ incident0d);
 
-        //SecruityAlert from Entity
-        String incident0a0 = sentinelRestApiCaller.get(HttpMethod.GET, ResourceType.ENTITIES, "1e203f20-c065-d5d3-65cb-c0d68bfa5001", Optional.empty());
+        //Get all Entity from a workspace
+        String incident0a0 = sentinelRestApiCaller.get(HttpMethod.GET, ResourceType.ENTITIES, Strings.EMPTY, Optional.empty());
         System.out.println(incident0a0);
     }
 }
