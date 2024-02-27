865e1bc5049822ebd4d53a0707df866ba080f006
Romuald Lemesle <romuald.lemesle@filigran.io>
2024-02-27 16:04:46 +0100

    Remove existing sentinel

--- openbas-sentinel/src/main/java/io/openbas/collectors/sentinel/domain/Property.java
+++
@@ -1,6 +0,0 @@
-package io.openbas.collectors.sentinel.domain;
-
-import lombok.Builder;
-
-@Builder
-public record Property(String label, String value) {}
--- openbas-sentinel/src/main/java/io/openbas/collectors/sentinel/domain/Provider.java
+++
@@ -1,6 +0,0 @@
-package io.openbas.collectors.sentinel.domain;
-
-import lombok.Builder;
-
-@Builder
-public record Provider(String id, String name) {}
