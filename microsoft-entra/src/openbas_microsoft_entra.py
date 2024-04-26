f30a35b9b7b457347228a442c257c458998a057c
Samuel Hassine <samuel.hassine@filigran.io>
2024-04-26 22:03:42 +0200

    [all] Fix default config

--- microsoft-entra/src/openbas_microsoft_entra.py
+++ microsoft-entra/src/openbas_microsoft_entra.py
@@ -135,9 +135,7 @@ class OpenBASMicrosoftEntra:
 
     # Start the main loop
     def start(self):
-        period = self.config.get_conf(
-            "collector_period", default="3600", is_number=True
-        )
+        period = self.config.get_conf("collector_period", default=3600, is_number=True)
         self.helper.schedule(message_callback=self._process_message, delay=period)
 
 
