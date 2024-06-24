9e73d341581aca5a56ac6fed01d5f29150eeb5d4
savacano28 <stephanya.casanova@filigran.io>
2024-06-24 08:50:12 +0200

    [crowdstrike] refact

---
+++ crowdstrike/src/collector/utils.py
@@ -0,0 +1,13 @@
+######################
+# CROWDSTRIKE UTILS #
+######################
+
+class Utils:
+    @staticmethod
+    def format_ioc(ioc_type, ioc):
+        return {
+          "type": ioc_type,
+          "value": ioc["value"],
+          "description": ioc.get("description", ""),
+        }
+
--- crowdstrike/src/crowdstrike/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""OpenBAS CrowdStrike collector module."""
-
-from crowdstrike.core import CrowdStrike
-
-__all__ = ["CrowdStrike"]
