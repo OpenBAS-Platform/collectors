855eecddf5369420063065838348029b172daa61
savacano28 <stephanya.casanova@filigran.io>
2024-06-24 09:01:27 +0200

    [crowdstrike] refact

--- crowdstrike/src/collector/openbas_crowdstrike.py
+++ crowdstrike/src/collector/openbas_crowdstrike.py
@@ -6,31 +6,64 @@ from .crowdstrike_api_handler import CrowdstrikeApiHandler
 class OpenBASCrowdStrike:
     def __init__(self):
         self.config = OpenBASConfigHelper(
-          __file__,
-          {
-            # API information
-            "openbas_url": {"env": "OPENBAS_URL", "file_path": ["openbas", "url"]},
-            "openbas_token": {"env": "OPENBAS_TOKEN", "file_path": ["openbas", "token"]},
-            # Config information
-            "collector_id": {"env": "COLLECTOR_ID", "file_path": ["collector", "id"]},
-            "collector_name": {"env": "COLLECTOR_NAME", "file_path": ["collector", "name"]},
-            "collector_type": {"env": "COLLECTOR_TYPE", "file_path": ["collector", "type"], "default": "openbas_crowdstream"},
-            "collector_period": {"env": "COLLECTOR_PERIOD", "file_path": ["collector", "period"]},
-            "collector_log_level": {"env": "COLLECTOR_LOG_LEVEL", "file_path": ["collector", "log_level"]},
-            # CrowdStrike
-            "crowdstrike_client_id": {"env": "CROWDSTRIKE_CLIENT_ID", "file_path": ["crowdstrike", "client_id"], "default": "CHANGEME"},
-            "crowdstrike_client_secret": {"env": "CROWDSTRIKE_CLIENT_SECRET", "file_path": ["crowdstrike", "client_secret"], "default": "CHANGEME"},
-            "crowdstrike_api_base_url": {"env": "CROWDSTRIKE_API_BASE_URL", "file_path": ["crowdstrike", "api_base_url"], "default": "https://api.crowdstrike.com"},
-          },
+            __file__,
+            {
+                # API information
+                "openbas_url": {"env": "OPENBAS_URL", "file_path": ["openbas", "url"]},
+                "openbas_token": {
+                    "env": "OPENBAS_TOKEN",
+                    "file_path": ["openbas", "token"],
+                },
+                # Config information
+                "collector_id": {
+                    "env": "COLLECTOR_ID",
+                    "file_path": ["collector", "id"],
+                },
+                "collector_name": {
+                    "env": "COLLECTOR_NAME",
+                    "file_path": ["collector", "name"],
+                },
+                "collector_type": {
+                    "env": "COLLECTOR_TYPE",
+                    "file_path": ["collector", "type"],
+                    "default": "openbas_crowdstream",
+                },
+                "collector_period": {
+                    "env": "COLLECTOR_PERIOD",
+                    "file_path": ["collector", "period"],
+                },
+                "collector_log_level": {
+                    "env": "COLLECTOR_LOG_LEVEL",
+                    "file_path": ["collector", "log_level"],
+                },
+                # CrowdStrike
+                "crowdstrike_client_id": {
+                    "env": "CROWDSTRIKE_CLIENT_ID",
+                    "file_path": ["crowdstrike", "client_id"],
+                    "default": "CHANGEME",
+                },
+                "crowdstrike_client_secret": {
+                    "env": "CROWDSTRIKE_CLIENT_SECRET",
+                    "file_path": ["crowdstrike", "client_secret"],
+                    "default": "CHANGEME",
+                },
+                "crowdstrike_api_base_url": {
+                    "env": "CROWDSTRIKE_API_BASE_URL",
+                    "file_path": ["crowdstrike", "api_base_url"],
+                    "default": "https://api.crowdstrike.com",
+                },
+            },
         )
-        self.helper = OpenBASCollectorHelper(self.config, open("img/icon-crowdstrike.png", "rb"))
+        self.helper = OpenBASCollectorHelper(
+            self.config, open("img/icon-crowdstrike.png", "rb")
+        )
 
         # Initialize CrowdStrike API
         self.crowdstrike_api_handler = CrowdstrikeApiHandler(
-          self.helper,
-          self.config.get_conf("crowdstrike_client_id"),
-          self.config.get_conf("crowdstrike_client_secret"),
-          self.config.get_conf("crowdstrike_api_base_url"),
+            self.helper,
+            self.config.get_conf("crowdstrike_client_id"),
+            self.config.get_conf("crowdstrike_client_secret"),
+            self.config.get_conf("crowdstrike_api_base_url"),
         )
 
     def _match_expectations(self):
@@ -51,4 +84,3 @@ class OpenBASCrowdStrike:
     def start(self):
         period = self.config.get_conf("collector_period", True, 60)
         self.helper.schedule(self._process_messages, period)
-
--- crowdstrike/src/collector/utils.py
+++ crowdstrike/src/collector/utils.py
@@ -2,12 +2,12 @@
 # CROWDSTRIKE UTILS #
 ######################
 
+
 class Utils:
     @staticmethod
     def format_ioc(ioc_type, ioc):
         return {
-          "type": ioc_type,
-          "value": ioc["value"],
-          "description": ioc.get("description", ""),
+            "type": ioc_type,
+            "value": ioc["value"],
+            "description": ioc.get("description", ""),
         }
-
