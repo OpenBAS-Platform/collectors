1e735cf08b910e0d4823ae43470898c388da8854
savacano28 <stephanya.casanova@filigran.io>
2024-06-25 18:10:14 +0200

    [crowdstrike] clean

--- crowdstrike/src/collector/openbas_crowdstrike.py
+++ crowdstrike/src/collector/openbas_crowdstrike.py
@@ -3,7 +3,6 @@ from datetime import datetime
 import pytz
 from dateutil.parser import parse
 from dateutil.relativedelta import relativedelta
-
 from pyobas.helpers import OpenBASCollectorHelper, OpenBASConfigHelper
 
 from .crowdstrike_api_handler import CrowdstrikeApiHandler
